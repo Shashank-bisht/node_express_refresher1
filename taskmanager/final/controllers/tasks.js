42eef3820b76b4e51a0ccb2728a6f206e291bfbf
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2023-10-09 12:02:37 +0530

    second commit

--- taskmanager/final/controllers/tasks.js
+++ taskmanager/final/controllers/tasks.js
@@ -1,12 +1,24 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) =>{
-    res.send("all items")
+const getAllTasks = async (req, res) =>{
+try {
+    const tasks = await Task.find({})
+    res.status(200).json({tasks})
+} catch (error) {
+    res.status(500).json({msg: error.message})
+}
 }
 
-const createTask = async (req, res) =>{
+const createTask = async (req, res) =>{ 
+    try {
+        // here Task is a model representing Task collection in database
+    //.create is used to create and save new documents
   const task = await Task.create(req.body)
   res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+    
 }
 const updateTask = (req, res) =>{
     res.send("all items")
--- taskmanager/final/models/Task.js
+++ taskmanager/final/models/Task.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    name:{
+        type:String,
+        required:[true, 'must provide a name'],
+        trim:true,
+        maxlength:[20,'name cannot be more than 20 characters']
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    },
 });
 
+//In Mongoose, a model is a constructor function that represents a specific collection in your MongoDB database. Models are created based on schemas and provide an interface for interacting with documents (data) within that collection
 module.exports = mongoose.model('Task',TaskSchema);
+// here Task is the name of the mongoose model
