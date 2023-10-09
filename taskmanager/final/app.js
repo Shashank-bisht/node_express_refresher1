e331d4103250f3b075c5b4c1dcfde2e75bca9164
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2023-10-09 17:28:49 +0530

    done with task manager backend

--- taskmanager/final/app.js
+++ taskmanager/final/app.js
@@ -2,19 +2,18 @@ require('./db/connect')
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found')
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
 require('dotenv').config();
 // importing routes 
 const tasks = require('./routes/tasks');
 
 
-app.get('/hello',(req,res)=>{
-    res.send("hello")
-})
-
 // all routes defined in the "tasks" module will be prefixed with "/api/v1/tasks
 app.use('/api/v1/tasks',tasks)
+app.use(notFound);
 // using app.use() to mount these routes under the "/api/v1/tasks" URL path
 
 const port = 8080
--- taskmanager/final/controllers/tasks.js
+++ taskmanager/final/controllers/tasks.js
@@ -1,5 +1,7 @@
 const Task = require('../models/Task')
 
+
+// getting all the tasks
 const getAllTasks = async (req, res) =>{
 try {
     const tasks = await Task.find({})
@@ -9,6 +11,7 @@ try {
 }
 }
 
+// creating tasks
 const createTask = async (req, res) =>{ 
     try {
         // here Task is a model representing Task collection in database
@@ -20,15 +23,58 @@ const createTask = async (req, res) =>{
     }
     
 }
-const updateTask = (req, res) =>{
-    res.send("all items")
+
+
+const updateTask = async (req, res) =>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body,{
+            // it will return new tasks
+            new: true,runValidators: true
+        })
+
+        if(!task){
+            return res.status(404).json({msg: "Task not found"})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
-const deleteTask = (req, res) =>{
-    res.send("all items")
+
+// delete tasks
+const deleteTask =async (req, res) =>{
+    try {
+        //naming id to taskID
+        const {id:taskID} = req.params; 
+        const task = await Task.findOneAndDelete({_id:taskID})
+        // naming _id to taskID
+        if(!task){
+            return res.status(404).json({msg: "Task not found"})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
-const getTask = (req, res) =>{
-    res.send("all items")
+
+// get a single task
+const getTask = async (req, res) =>{
+try {
+    // destructuring id and naming it to taskID 
+    const {id:taskID} = req.params
+    const task = await Task.findOne({_id:taskID})
+    if(!task){
+        return res.status(404).json({msg: "Task not found"})
+    }
+    res.status(200).json({task})
+} catch (error) {
+    // for syntax errors
+    res.status(500).json({msg:error})
 }
+}
+
 
 
 module.exports = {getAllTasks, createTask,deleteTask,getTask,updateTask}
