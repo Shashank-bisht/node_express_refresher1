6ce77619fea6cc4e6aa5a4f612145965213fa77b
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2024-01-10 19:18:21 +0530

    played with api

--- jobsapi/middleware/authentication.js
+++ jobsapi/middleware/authentication.js
@@ -8,11 +8,12 @@ const auth = (req, res, next) => {
     if(!authHeader || !authHeader.startsWith('Bearer')){
         throw new UnauthenticatedError("Authentication invalid")
     }
-    //f the Authorization header is present and correctly formatted, the code extracts the JWT from the header by splitting it at the space (' ') and taking the second part (the token).
+    //if the Authorization header is present and correctly formatted, the code extracts the JWT from the header by splitting it at the space (' ') and taking the second part (the token).
     const token = authHeader.split(' ')[1]
     try {
         //The extracted token is then passed to the jwt.verify method to verify its authenticity. It uses a secret key (process.env.JWT_SECRET) to check whether the token has been tampered with and to decode the token's payload.
         const payload = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(payload)
         // attach the user to the job routes
         req.user = {userId: payload.userId, name:payload.name}
         next()
